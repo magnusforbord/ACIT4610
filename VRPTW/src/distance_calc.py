a35d700050f6d195c2f7b6f8907a5f48bf7c359b
jintn <jintonyn@gmail.com>
2024-11-12 12:45:38 +0100

    removed old vrptw solution

--- VRPTW/src/distance_calc.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from problem_model import Customer
-
-def euclidean_distance(customer_1: Customer, customer_2: Customer) -> float:
-    """Calculate the Euclidean distance between two Customer objects."""
-    return np.sqrt((customer_1.x_coord - customer_2.x_coord) ** 2 +
-                   (customer_1.y_coord - customer_2.y_coord) ** 2)
